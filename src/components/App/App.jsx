1d5a5b1a9e885d5dbdfb2453d1d3abd92fc0401e
xb <cherepanov92@gmail.com>
2022-08-06 23:36:03 +0500

    Написал шапку проекта

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import styles from './App.module.css';
+import { AppHeader } from "../AppHeader";
 
 function App() {
     return (
         <div className="App">
+            <AppHeader />
         </div>
     );
 }
---
+++ src/components/AppHeader/AppHeader.jsx
@@ -0,0 +1,27 @@
+import { Logo } from '@ya.praktikum/react-developer-burger-ui-components';
+import React, { useState } from 'react';
+
+import styles from './AppHeader.module.css';
+import { NavLink, CONSTRUCTOR, LK, ORDER_QUEUE } from "./NavLink";
+import classNames from "classnames";
+
+export const AppHeader = () => {
+    const [activePage, setActivePage] = useState(CONSTRUCTOR);
+
+    return (
+        <header className={classNames(styles.wrapper, "p-5")}>
+            <nav className={styles.nav}>
+                <section className={styles.leftBlock}>
+                    <NavLink type={CONSTRUCTOR} activeType={activePage} text={"Конструктор"} setActiveType={setActivePage} />
+                    <NavLink type={ORDER_QUEUE} activeType={activePage} text={"Лента заказов"} setActiveType={setActivePage} />
+                </section>
+                <section className={styles.centralBlock}>
+                    <Logo />
+                </section>
+                <section className={styles.rightBlock}>
+                    <NavLink type={LK} activeType={activePage} text={"Личный кабинет"} setActiveType={setActivePage} />
+                </section>
+            </nav>
+        </header>
+    );
+};
