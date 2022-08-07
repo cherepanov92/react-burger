0875cadd675e73cf2bd2a8bd53c3cdbfee96cd0c
xb <cherepanov92@gmail.com>
2022-08-07 14:57:45 +0500

    Написал компонент отображения списка ингредиентов

---
+++ src/components/BurgerIngredients/IngredientsBlock/IngredientsBlock.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './IngredientsBlock.module.css';
+import { IngredientItem } from "../IngredientItem";
+import classNames from "classnames";
+
+
+export const IngredientsBlock = ({title, ingredients}) => {
+    return (
+        <>
+            <p className="text text_type_main-medium">{title}</p>
+            <div className={classNames(styles.ingredientsBlock ,"pt-6 pr-2 pb-7 pl-2")} style={{backgroundColor: "red"}}>
+                {ingredients.map(item => (
+                    <IngredientItem key={item._id} data={item} />
+                ))}
+            </div>
+        </>
+    );
+};
