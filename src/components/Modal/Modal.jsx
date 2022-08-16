99fbb42ad7afd0fd48dc4ccc4d370194f52d6132
xb <cherepanov92@gmail.com>
2022-08-16 11:22:46 +0500

    Выпилил лишнюю обёртку компонента в фрагмент

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -23,19 +23,17 @@ const Modal  = ({ title, onClose, children }) => {
     const stopPropagation = (e) => {e.stopPropagation()}
 
     return ReactDOM.createPortal(
-        <>
-            <ModalOverlay onClose={onClose} >
-                <div className={`pt-10 pl-10 pr-10 ${styles.wrapper}`} onClick={stopPropagation}>
-                    <div className={styles.header}>
-                        <h3 className={`text text_type_main-large ${styles.title}`}>{title}</h3>
-                        <button onClick={onClose} className={`pt-5 ${styles.button}`}>
-                            <CloseIcon type='primary' />
-                        </button>
-                    </div>
-                    {children}
+        <ModalOverlay onClose={onClose} >
+            <div className={`pt-10 pl-10 pr-10 ${styles.wrapper}`} onClick={stopPropagation}>
+                <div className={styles.header}>
+                    <h3 className={`text text_type_main-large ${styles.title}`}>{title}</h3>
+                    <button onClick={onClose} className={`pt-5 ${styles.button}`}>
+                        <CloseIcon type='primary' />
+                    </button>
                 </div>
-            </ModalOverlay>
-        </>,
+                {children}
+            </div>
+        </ModalOverlay>,
         modalRoot
     );
 };
