d22d5f35da6649ec53ee4d9fb025a377aba32233
xb <cherepanov92@gmail.com>
2022-08-20 18:39:21 +0500

    Отображаю данные из контекста в BurgerConstructor

--- src/components/BurgerConstructor/BurgerConstructor.jsx
+++ src/components/BurgerConstructor/BurgerConstructor.jsx
@@ -1,57 +1,67 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import classNames from "classnames";
 import { Button, ConstructorElement, CurrencyIcon, DragIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import PropTypes from "prop-types";
 
 import styles from './BurgerConstructor.module.css';
 import { OrderDetails } from "./OrderDetails";
-import { ingredientType } from "../../utils/types";
-import { getIngredientsGroups } from "../../utils/getIngredientsGroups";
+import { BurgerContext } from "../../context/BurgerContextProvider";
 
-export const BurgerConstructor = ({ ingredients, attachModal, onClose }) => {
-    const { bun, main, sauce } = getIngredientsGroups(ingredients)
-    const basicBun = bun[0];
+export const BurgerConstructor = ({ attachModal, onClose }) => {
+    const { orderIngredients } = useContext(BurgerContext);
+    const basicBun = orderIngredients.bun[0];
+    const totalPrice = orderIngredients.totalPrice
 
     return (
         <section className={styles.wrapper}>
             <section className={classNames(styles.ingredientsBlock, "pt-25")}>
-                <div className={'mr-4'}>
-                    <ConstructorElement
-                        type={'top'}
-                        isLocked={true}
-                        text={`${basicBun.name} (верх)`}
-                        price={basicBun.price}
-                        thumbnail={basicBun.image}
-                    />
-                </div>
-                <div className={styles.selectedBlock}>
-                    {[...sauce, ...main].map(item => (
-                        <div key={item._id} className={classNames(styles.ingredient, 'mr-2')}>
-                            <DragIcon type="primary" />
+                {totalPrice ? (
+                    <>
+                        {basicBun && <div className={'mr-4'}>
                             <ConstructorElement
-                                text={item.name}
-                                price={item.price}
-                                thumbnail={item.image}
+                                type={'top'}
+                                isLocked={true}
+                                text={`${basicBun.name} (верх)`}
+                                price={basicBun.price}
+                                thumbnail={basicBun.image}
                             />
+                        </div>}
+                        <div className={styles.selectedBlock}>
+                            {[...orderIngredients.sauce, ...orderIngredients.main].map(item => (
+                                <div key={item._id} className={classNames(styles.ingredient, 'mr-2')}>
+                                    <DragIcon type="primary" />
+                                    <ConstructorElement
+                                        text={item.name}
+                                        price={item.price}
+                                        thumbnail={item.image}
+                                    />
+                                </div>
+                            ))}
                         </div>
-                    ))}
-                </div>
-                <div className={'mr-4'}>
-                    <ConstructorElement
-                        type={'bottom'}
-                        isLocked={true}
-                        text={`${basicBun.name} (низ)`}
-                        price={basicBun.price}
-                        thumbnail={basicBun.image}
-                    />
-                </div>
+                        { basicBun && <div className={'mr-4'}>
+                            <ConstructorElement
+                                type={'bottom'}
+                                isLocked={true}
+                                text={`${basicBun.name} (низ)`}
+                                price={basicBun.price}
+                                thumbnail={basicBun.image}
+                            />
+                        </div>}
+                    </>
+                ) : (
+                    <p>Пусто</p>
+                )}
             </section>
             <section className={classNames(styles.priceBlock, "mt-10 mr-4 mb-10")}>
                 <div className={"mr-10"}>
-                    <span className="text text_type_digits-medium mr-2">610</span>
+                    <span className="text text_type_digits-medium mr-2">{totalPrice}</span>
                     <CurrencyIcon type="primary" />
                 </div>
-                <Button onClick={() => attachModal(<OrderDetails onClose={onClose} />)} type="primary" size="large">
+                <Button
+                    onClick={() => attachModal(<OrderDetails onClose={onClose} />)}
+                    type={totalPrice ? "primary" : "disable"}
+                    size="large"
+                >
                     Оформить заказ
                 </Button>
             </section>
@@ -60,7 +70,6 @@ export const BurgerConstructor = ({ ingredients, attachModal, onClose }) => {
 };
 
 BurgerConstructor.propTypes = {
-    ingredients: PropTypes.arrayOf(ingredientType.isRequired).isRequired,
     attachModal: PropTypes.func.isRequired,
     onClose: PropTypes.func.isRequired
 };
