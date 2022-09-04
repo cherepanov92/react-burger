1637a1878aab4ee080fb853e1df18d3f3d77b7da
xb <cherepanov92@gmail.com>
2022-09-04 23:30:22 +0500

    Поправил логику отображения кол-ва ингредиента в заказе

--- src/components/BurgerIngredients/IngredientItem/IngredientItem.jsx
+++ src/components/BurgerIngredients/IngredientItem/IngredientItem.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import classNames from "classnames";
-import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { Counter, CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 
 import styles from './IngredientItem.module.css';
 import { ingredientType } from "../../../utils/types";
@@ -11,16 +11,27 @@ import { ADD_INGREDIENT_DETAILS } from "../../../services/actions/IngredientDeta
 export const IngredientItem = ({ingredient}) => {
     const dispatch = useDispatch();
     const { bun, ingredients } = useSelector(state => state.constructor);
+    const [ count, setCount ] = useState(0);
 
     const showIngredientDetails = () => {
         dispatch({ type: ADD_INGREDIENT_DETAILS, ingredient: ingredient })
     }
 
-    const count = orderIngredients[ingredient.type].filter(item => item._id === ingredient._id ).length
+    useEffect(() => {
+        setCount(
+            ingredient.type === 'bun' ? (
+                bun._id === ingredient._id && 2
+            ) : (
+                ingredients.filter(item => item._id === ingredient._id ).length
+            )
+        )
+    }, [ ingredient, bun, ingredients ])
+
     return (
         <div
             onClick={() => showIngredientDetails()}
             className={classNames(styles.wrapper, 'mb-4')}>
+            { !!count && <Counter count={ count } size="default" />}
             <img className={"ml-4 mr-4 mb-1"} src={ingredient.image} alt={`Компонент: ${ingredient.name}`}/>
             <div className={classNames(styles.priceBlock, "pt-1 pb-1")}>
                 <span className="text text_type_digits-default mr-2">{ingredient.price}</span>
