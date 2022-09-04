13455b04f0eff387b39f5433c6e64f224e101f99
xb <cherepanov92@gmail.com>
2022-09-05 01:22:51 +0500

    Выпилил остатки контекста

--- src/index.jsx
+++ src/index.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./components/App/App";
-import { BurgerContextProvider } from "./context/BurgerContextProvider";
 import { Provider } from "react-redux";
 import createStore from './services/reducers/Root';
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={createStore()}>
-            <BurgerContextProvider>
-                <App />
-            </BurgerContextProvider>
+            <App />
         </Provider>
     </React.StrictMode>,
     document.getElementById('root')
