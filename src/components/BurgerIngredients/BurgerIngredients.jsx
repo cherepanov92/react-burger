51ab3f7ccfa8e5592e1fa18f7c1d34c412c6fef2
xb <cherepanov92@gmail.com>
2022-09-22 01:32:01 +0500

    Связал табы списка ингредиентов с его группами

--- src/components/BurgerIngredients/BurgerIngredients.jsx
+++ src/components/BurgerIngredients/BurgerIngredients.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import { useDispatch, useSelector } from "react-redux";
 
 import styles from './BurgerIngredients.module.css';
@@ -9,11 +9,51 @@ import { getIngredientsData } from "../../services/actions/Ingredients";
 export const BurgerIngredients = () => {
     const dispatch = useDispatch();
     const { ingredients, ingredientsRequest } = useSelector(state => state.ingredients);
+    const [currentZone, setCurrentZone] = useState('bun');
+
+    const bunRef = useRef(null);
+    const sauceRef = useRef(null);
+    const mainRef = useRef(null);
 
     useEffect(() => {
         dispatch(getIngredientsData());
         }, [dispatch]
     );
+
+    const handleScroll = (e) => {
+        e.stopPropagation();
+        const currentPosition = e.currentTarget.scrollTop + e.currentTarget.offsetTop;
+        const bufZone = (sauceRef.current.offsetTop - bunRef.current.offsetTop) / 2 + bunRef.current.offsetTop;
+        const mainZone = (mainRef.current.offsetTop - sauceRef.current.offsetTop) / 2 + sauceRef.current.offsetTop;
+
+        //todo: будем в TS переписать на enum
+        if (currentPosition <= bufZone) {
+            setCurrentZone('bun')
+        }
+        else if (currentPosition >= mainZone) {
+            setCurrentZone('main')
+        }
+        else {
+            setCurrentZone('sauce')
+        }
+    }
+
+    const changeCurrentZone = (zone) => {
+        //todo: будем в TS переписать на enum
+        switch (zone) {
+            case 'bun':
+                bunRef.current.scrollIntoView();
+                return;
+            case 'main':
+                mainRef.current.scrollIntoView();
+                return;
+            case 'sauce':
+                sauceRef.current.scrollIntoView();
+                return;
+            default:
+                return;
+        }
+    }
 
 
     return (
@@ -24,18 +64,24 @@ export const BurgerIngredients = () => {
                 </p>
             </div>
             <div className={'pb-10'}>
-                <IngredientsTabs />
+                <IngredientsTabs current={currentZone} changeCurrentZone={changeCurrentZone} />
             </div>
-            <div className={styles.ingredientsBlock}>
+            <div className={styles.ingredientsBlock} onScroll={handleScroll}>
                 {ingredientsRequest ? (
                     <p className="text text_type_main-large mt-5">
                         Loading ...
                     </p>
                 ) : (
                     <>
-                        <IngredientsBlock title={'Булки'} ingredients={ingredients.bun} />
-                        <IngredientsBlock title={'Соусы'} ingredients={ingredients.sauce} />
-                        <IngredientsBlock title={'Начинки'} ingredients={ingredients.main} />
+                        <div ref={bunRef}>
+                            <IngredientsBlock title={'Булки'} ingredients={ingredients.bun} />
+                        </div>
+                        <div ref={sauceRef}>
+                            <IngredientsBlock title={'Соусы'} ingredients={ingredients.sauce} />
+                        </div>
+                        <div ref={mainRef}>
+                            <IngredientsBlock title={'Начинки'} ingredients={ingredients.main} />
+                        </div>
                     </>
                 )}
             </div>
