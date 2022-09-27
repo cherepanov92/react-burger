5e202f56599811377ffb58055117021014cd3bd1
xb <cherepanov92@gmail.com>
2022-09-28 00:55:24 +0500

    Добавляю всем ингредиентам конструктора orderId для корректного удаления конкретного элемента

--- src/services/reducers/Constructor.js
+++ src/services/reducers/Constructor.js
@@ -4,19 +4,22 @@ export default function ConstructorReducer(state = {}, action) {
 
     switch (action.type) {
         case ADD_INGREDIENT:
-            if (action.ingredient.type === 'bun') {
+            const ingredientWithOrderHash = {...action.ingredient, orderId: Math.floor(Math.random() * 10000000)};
+
+            if (ingredientWithOrderHash.type === 'bun') {
                 const totalPriceWithoutBun = state.bun ? state.totalPrice - (state.bun.price * 2) : state.totalPrice;
 
                 return {
                     ...state,
-                    bun: action.ingredient,
-                    totalPrice: totalPriceWithoutBun + (action.ingredient.price * 2)
+                    bun: ingredientWithOrderHash,
+                    totalPrice: totalPriceWithoutBun + (ingredientWithOrderHash.price * 2)
                 }
             }
+
             return {
                 ...state,
-                ingredients: [...state.ingredients, action.ingredient],
-                totalPrice: state.totalPrice + action.ingredient.price
+                ingredients: [...state.ingredients, ingredientWithOrderHash],
+                totalPrice: state.totalPrice + ingredientWithOrderHash.price
             }
         case REMOVE_INGREDIENT:
             if (!state.ingredients.length) {
@@ -25,7 +28,7 @@ export default function ConstructorReducer(state = {}, action) {
 
             return {
                 ...state,
-                ingredients: [...state.ingredients.filter(ingredient => ingredient._id !== action.ingredient._id)],
+                ingredients: [...state.ingredients.filter(ingredient => ingredient.orderId !== action.ingredient.orderId)],
                 totalPrice: state.totalPrice - action.ingredient.price
             }
         case MOVE_INGREDIENT:
