d909d50b2519179adb99064485a60d42853120c5
xb <cherepanov92@gmail.com>
2022-10-27 00:48:14 +0500

    Добавил базовый нижний отступ

--- src/components/Modal/ErrorModal/ErrorModal.jsx
+++ src/components/Modal/ErrorModal/ErrorModal.jsx
@@ -16,9 +16,9 @@ export const ErrorModal = () => {
 
     return (
         <Modal onClose={closeModal} title={'Ошибка'}>
-            <div className={styles.wrapper}>
-                {status && <span className={classNames('text text_type_main-medium mt-3 mb-3 pb-10')}>Статус:{status}</span>}
-                {message && <span className={classNames('text text_type_main-medium mt-3 mb-3 pb-10')}>Сообщение:{message}</span>}
+            <div className={classNames(styles.wrapper, 'pb-10')}>
+                {status && <span className={classNames('text text_type_main-medium mt-3 mb-3')}>Статус:{status}</span>}
+                {message && <span className={classNames('text text_type_main-medium mt-3 mb-3')}>Сообщение:{message}</span>}
             </div>
         </Modal>
     );
