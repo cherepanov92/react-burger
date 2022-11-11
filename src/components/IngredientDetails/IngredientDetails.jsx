00ef5b535f756ac3b0130335814a4c75b87e70cf
xb <cherepanov92@gmail.com>
2022-11-12 02:28:30 +0500

    Добавил лоадер на ожидание данных ингредиента

--- src/components/IngredientDetails/IngredientDetails.jsx
+++ src/components/IngredientDetails/IngredientDetails.jsx
@@ -3,30 +3,43 @@ import classNames from 'classnames';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
-import styles from './IngredientDetails.module.css';
 import { getIngredientsData } from '../../services/actions/Ingredients';
 
-const IngredientDetailsContent = ({ ingredient }) => {
+import styles from './IngredientDetails.module.css';
+
+const IngredientDetailsContent = ({ ingredient, isLoading }) => {
+    const getProperty = propertyName => {
+        return isLoading ? '...' : ingredient[propertyName];
+    };
+
     return (
         <div className={styles.wrapper}>
-            <img src={ingredient.image_large} alt={ingredient.name} />
-            <p className={'text text_type_main-medium pt-3 pb-3'}>{ingredient.name}</p>
+            {isLoading ? (
+                <div className={styles.loaderContainer}>
+                    <span className={styles.loader}></span>
+                </div>
+            ) : (
+                <img src={ingredient.image_large} alt={ingredient.name} />
+            )}
+            <p className={'text text_type_main-medium pt-3 pb-3'}>{!isLoading && ingredient.name}</p>
             <div className={classNames('pt-5 pb-15', styles.propertyList)}>
                 <div className={styles.propertyItem}>
                     <p className={'text text_type_main-default text_color_inactive mb-2'}>Калории,ккал</p>
-                    <p className={'text text_type_digits-default text_color_inactive'}>{ingredient.calories}</p>
+                    <p className={'text text_type_digits-default text_color_inactive'}>{getProperty('calories')}</p>
                 </div>
                 <div className={styles.propertyItem}>
                     <p className={'text text_type_main-default text_color_inactive mb-2'}>Белки, г</p>
-                    <p className={'text text_type_digits-default text_color_inactive'}>{ingredient.proteins}</p>
+                    <p className={'text text_type_digits-default text_color_inactive'}>{getProperty('proteins')}</p>
                 </div>
                 <div className={styles.propertyItem}>
                     <p className={'text text_type_main-default text_color_inactive mb-2'}>Жиры, г</p>
-                    <p className={'text text_type_digits-default text_color_inactive'}>{ingredient.fat}</p>
+                    <p className={'text text_type_digits-default text_color_inactive'}>{getProperty('fat')}</p>
                 </div>
                 <div className={styles.propertyItem}>
                     <p className={'text text_type_main-default text_color_inactive mb-2'}>Углеводы, г</p>
-                    <p className={'text text_type_digits-default text_color_inactive'}>{ingredient.carbohydrates}</p>
+                    <p className={'text text_type_digits-default text_color_inactive'}>
+                        {getProperty('carbohydrates')}
+                    </p>
                 </div>
             </div>
         </div>
@@ -37,11 +50,7 @@ const IngredientDetails = ({ ingredient }) => {
     const [isLoading, setIsLoading] = useState(true);
     useEffect(() => setIsLoading(ingredient === null), [ingredient]);
 
-    if (isLoading) {
-        return <p>Loading</p>;
-    }
-
-    return <IngredientDetailsContent ingredient={ingredient} />;
+    return <IngredientDetailsContent ingredient={ingredient} isLoading={isLoading} />;
 };
 
 const IngredientDetailsContainer = ({ useModal }) => {
