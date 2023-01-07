65f4362481bdd1d1a7896b2a6861cfb0a7ee0d4b
xb <cherepanov92@gmail.com>
2023-01-07 14:40:47 +0500

    Заполняю список заказов в профиле

--- src/pages/ProfilePage/Orders/Orders.tsx
+++ src/pages/ProfilePage/Orders/Orders.tsx
@@ -2,11 +2,17 @@ import React, { FC } from 'react';
 import { OrderList } from '../../../components/Feed';
 import styles from './Ordres.module.css';
 import classNames from 'classnames';
+import {useAppSelector} from "../../../services/reducers/Root";
 
 const Orders: FC = () => {
+    const { orderData, ingredients } = useAppSelector(state => ({
+        orderData: state.orderList.data,
+        ingredients: state.ingredients.ingredients
+    }));
+
     return (
         <section className={classNames(styles.orderList, 'mt-15')}>
-            <OrderList />
+            <OrderList orderData={orderData} ingredients={ingredients} />
         </section>
     );
 };
